35007cbe0cf9ca520d9ee3999dfc68bab4de9e27
mxwtnb <>
2022-01-14 13:05:06 +0000

    Remove unused interface

--- interfaces/IStrategy.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-
-pragma solidity 0.7.6;
-
-interface IStrategy {
-    function rebalance() external;
-
-    function shouldRebalance() external view returns (bool);
-}
